796d49f228e4297236b95d243c3589dfa83c38be
Kwadwo-900 <sagudze900@gmail.com>
2023-03-27 20:49:23 +0000

    slight change

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sagudze900@gmail.com'
-EMAIL_HOST_PASSWORD = 'nqmskqkgkapwgytd'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
