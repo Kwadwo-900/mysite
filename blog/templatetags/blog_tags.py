32c7f33a0da03df284f4ea7d6938474e442e476d
Kwadwo-900 <sagudze900@gmail.com>
2023-03-31 23:50:51 +0000

    rectified an error

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -14,5 +14,5 @@ def show_latest_posts(count = 5):
     return {'latest_posts':latest_posts}
 
 @register.simple_tag
-def get_most_commented_post(count = 5):
-    return Post.publish.annotate(total_comments = Count('comments')).order_by('-total_comments')[:count]
+def get_most_commented_posts(count = 5):
+    return Post.published.annotate(total_comments = Count('comments')).order_by('-total_comments')[:count]
