1704e0470c0f8bb676a2cf624b9145ab9b21ea7c
Kwadwo-900 <sagudze900@gmail.com>
2023-04-01 22:01:45 +0000

    final commit

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector,SearchQuery,SearchRank
+from django.contrib.postgres.search import SearchVector,SearchQuery,SearchRank, TrigramSimilarity
 
 # Create your views here.
 def post_list(request, tag_slug = None):
@@ -106,9 +106,18 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title','body')
-            search_query = SearchQuery(query)
-            results = Post.published.annotate(search = search_vector,
-                                              rank = SearchRank(search_vector,search_query)).filter(search = search_query).order_by('-rank')
+        results = Post.published.annotate(
+            similarity = TrigramSimilarity('title', query)
+        ).filter(rank__gte = 0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'form':form,'query':query,'results':results})
+
+#generate search results using by assigning weights
+''' if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title',weight = 'A') + SearchVector('body', weight = 'B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search = search_vector,
+                                              rank = SearchRank(search_vector,search_query)).filter(rank__gte = 0.3).order_by('-rank')'''
