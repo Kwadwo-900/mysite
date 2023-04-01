5a9fd85c108d7bb7094ac9c405d116060b516a18
Kwadwo-900 <sagudze900@gmail.com>
2023-04-01 20:37:20 +0000

    added search functionality

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,13 @@ from .models import Post,Comment
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm,CommentForm
+from .forms import EmailPostForm,CommentForm,SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector,SearchQuery,SearchRank
+
 # Create your views here.
 def post_list(request, tag_slug = None):
     post_list = Post.published.all()
@@ -94,3 +96,19 @@ def post_comment(request, post_id):
         comment.save()
 
     return render(request, 'blog/post/comment.html', {'post':post,'form':form,'comment':comment})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search = search_vector,
+                                              rank = SearchRank(search_vector,search_query)).filter(search = search_query).order_by('-rank')
+
+    return render(request, 'blog/post/search.html', {'form':form,'query':query,'results':results})
